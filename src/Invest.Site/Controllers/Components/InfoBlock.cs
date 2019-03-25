255301544de98e6d49284cc151e96ad66fa97d95
Alexander Yermakov <alexandr.v.ermakov@gmail.com>
2019-03-25 08:08:37 +0200

    Temporary commit

---
+++ src/Invest.Site/Controllers/Components/InfoBlock.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Invest.Site.Controllers.Components
+{
+	public class InfoBlock : ViewComponent
+	{
+		public async Task<IViewComponentResult> InvokeAsync ()
+		{
+			return View();
+		}
+	}
+}
