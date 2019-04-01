a0fb0869a840aead9dd9fac5d1195678f7f6a16b
CORP\alexander.ermakov <alexander.ermakov@muranosoft.com>
2019-04-01 10:55:14 +0300

    Temporaru commit

---
+++ src/Invest.Backend.Grains/GrainImplementations/CustomerGrain.cs
@@ -0,0 +1,8 @@
+﻿using Invest.Backend.Grains.GrainImplementations.StateModels;
+
+namespace Invest.Backend.Grains.GrainImplementations
+{
+	public class CustomerGrain : Grain<CustomerState>, ICustomerGrain
+	{
+	}
+}
